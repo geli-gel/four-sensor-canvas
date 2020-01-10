618ddbb390f6a56cd65f55517e900eb69d730f86
geli-gel <angele.zam@gmail.com>
2020-01-10 11:13:20 -0800

    removed excess comments from drawing.js

--- src/drawing.js
+++ src/drawing.js
@@ -17,7 +17,6 @@ class Drawing {
   wiggleAround() {
     this.x = this.x + this.p.random(-5,5);
     this.y = this.y + this.p.random(-5,5);
-    console.log('wiggled the drawing')
   }
 
   // to-do: for bonk - what to do if it's reached an edge
@@ -27,24 +26,16 @@ class Drawing {
 
     // this.p.ellipse(this.x, this.y, 5);
 
-
-
-
     // this.p.background(0,0,80);
 
-
-    
-    console.log('in Drawing, preparing to display the drawing')
-    console.log('this.lineData: ', this.lineData);
     this.p.stroke(200,200, 0);
     this.p.strokeWeight(4);
-    // this.p.noFill();
     this.p.beginShape(this.p.LINES);
     for (let lineParts of this.lineData) {
       // this.p.line(lineParts[0] + this.x, lineParts[1] + this.y , lineParts[2] + this.x, lineParts[3] + this.y);
       this.p.vertex(lineParts[0] + this.x, lineParts[1] + this.y);
     }
-    this.p.endShape(); // forgot this... bug fixed lol >:( uhhh, 
+    this.p.endShape();
   }
 
   update = (time) => { 
@@ -59,39 +50,3 @@ class Drawing {
 }
 
 export default Drawing;
-
-// old Drawing from following video
-
-// class Drawing {
-
-//   constructor(p, xStart, yStart, modelName){
-//     this.p = p
-//     this.x = xStart
-//     this.y = yStart
-//     //... kind of a work in progress...
-//     //   this.modelName = modelName
-//     console.log('drawing modelName from constructor: ', this.modelName)
-//   }
-
-
-//   move() {
-//     this.x = this.x + this.p.random(-5,5);
-//     this.y = this.y + this.p.random(-5,5);
-//     // to-do: make move take in a type parameter and do a case-switch with how to move it (i think?)
-//     // this.x += this.p.random(-this.speed, this.speed)
-//   }
-
-//   // to-do: for bonk - what to do if it's reached an edge
-
-
-//   display() {
-//     this.p.stroke(255);
-//     this.p.strokeWeight(4);
-//     this.p.noFill();
-//     this.p.ellipse(this.x, this.y, 20, 20);
-//     this.p.push()
-//     this.p.text('poopy',200,150)
-//   }
-// }
-
-// export default Drawing;

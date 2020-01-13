ced456365448c3b0146e02a5803bb50586a82aaf
geli-gel <angele.zam@gmail.com>
2020-01-13 12:07:31 -0800

    refactored App so that state can be set more easily

--- src/App.js
+++ src/App.js
@@ -8,55 +8,72 @@ class App extends Component {
     super(props)
     this.state = {
       sketchDetails: {
-        drawingModel: "bee",
-        drawingSize: "",
-        drawingAmount: 4,
-        drawingColor: "",
-        drawingAnimation: "wiggleAround",
+        "TOP": "",              // drawingModel (from reader)
+        "LEFT": "",    // drawingAnimation (from reader)
+        "RIGHT": 4,                // drawingAmount (from button)
+        "BOTTOM": "",              // drawingColor (from button)
+        drawingSize: 0.1,          // drawingSize (from a button?)
       },
 
-      topTokenDescriptions: {
-        1: "bee", // in the future these will be able to be set to whatever via dropdown in reader settings
-        2: "flower",
-        3: "strawberry",
-        4: "snail",
-        5: "castle",
-        6: "ant",
-      },
+      TokenDescriptions: {
+        "TOP": {
+          1: "bee", // in the future these will be able to be set to whatever via dropdown in reader settings
+          2: "flower",
+          3: "strawberry",
+          4: "snail",
+          5: "castle",
+          6: "ant",
+        },
 
-      leftTokenDescriptions: {
-        1: ["slow" , "falling"],
-        2: ["slow", "rising"],
-        3: ["slow", "flocking"],
-      },
+        "LEFT": { 
+          1: ["wiggleAround"],
+          2: ["flock"],
+          3: ["bonkAround"],
+        },
 
-      rightTokenDescriptions: {
-        1: ["small", "many"],
-        2: ["medium", "some"],
-        3: ["large", "few"],
-        4: ["mix", "some"],
-      },
+        // if only 2 readers, this will be on the app
+        "RIGHT": { // and a cycle of removing when new ones are added once at max
+          1: ["few"],     // 5
+          2: ["some"],    // 11
+          3: ["many"],    // 16
+          4: ["tooMany"], // 20 
+        },
 
-      bottomTokenDescriptions: {
-        1: "red", // it would be cool if when you change the token, new ones pop up in that color and slowly replace the old color (so drawings have lifetimes)
-        2: "orange",
-        3: "yellow",
-        4: "green",
-        5: "blue",
-        6: "purple",
-        7: "cyan",
-        8: "magenta",
-        9: "black",
-        10: "rainbow", // either each drawing is a random color, or better, each stroke of each drawing is a random color
+        // if only 2 readers, this will be on the app
+        "BOTTOM": { 
+          1: "red", // it would be cool if when you change the token, new ones pop up in that color and slowly replace the old color (so drawings have lifetimes)
+          2: "orange",
+          3: "yellow",
+          4: "green",
+          5: "blue",
+          6: "purple",
+          7: "cyan",
+          8: "magenta",
+          9: "black",
+          10: "rainbow", // either each drawing is a random color, or better, each stroke of each drawing is a random color
+        },
       },
       
+      //to-do: this is the same as sketch details except when it's empty so... yeah
       readerLabels: {
-        top: "Empty",
-        left: "Empty",
-        right: "Empty",
-        bottom: "Empty",  
+        "TOP": "Empty",
+        "LEFT": "Empty",
+        "RIGHT": "Empty",
+        "BOTTOM": "Empty",  
       }
     }
+  }
+
+  updateSketchDetails = (messageWord, messageNumber) => {
+    let updatedSketchDetails = this.state.sketchDetails;
+    updatedSketchDetails[messageWord] = this.state.TokenDescriptions[messageWord][messageNumber];
+    // to-do: again, sketchdetails is the same as reader labels...need to get rid of it i think
+    let updatedReaderLabels = this.state.readerLabels;
+    updatedReaderLabels[messageWord] = updatedSketchDetails[messageWord];
+    this.setState({
+      sketchDetails: updatedSketchDetails,
+      readerLabels: updatedReaderLabels,
+    })
   }
 
   onReaderMessage = (message) => {
@@ -65,39 +82,35 @@ class App extends Component {
 
     const messageWordMatch = message.match(lettersRegex);
     const messageNumberMatch = message.match(numbersRegex);
+
     console.log('onReaderMessage message contents (from App): ', message);
 
-    switch(messageWordMatch[0]) {
-      case "TOP":
-        console.log('in switch case "TOP"')
-        console.log('this.state', this.state)
-        if (messageNumberMatch) {
-          console.log('number match:', messageNumberMatch[0]);
-          if (this.state.topTokenDescriptions[messageNumberMatch[0]]) { // I'm saying if the message has a number that corresponds to a number in this reader's state description, set the sketch details and top reader label to that,
-            let updatedSketchDetails = this.state.sketchDetails;
-            updatedSketchDetails.drawingModel = this.state.topTokenDescriptions[messageNumberMatch[0]];
+    if (messageWordMatch && messageNumberMatch) {
+      const messageWord = messageWordMatch[0];
+      const messageNumber = messageNumberMatch[0];
 
-            let updatedReaderLabels = this.state.readerLabels;
-            updatedReaderLabels.top = updatedSketchDetails.drawingModel;
-            this.setState({
-              sketchDetails: updatedSketchDetails,
-              readerLabels: updatedReaderLabels,
-            })
-          }
+      if (this.state.TokenDescriptions[messageWord]) {
+        if (this.state.TokenDescriptions[messageWord][messageNumber]) { // I'm saying if the message has a number that corresponds to a number in this reader's state description, set the sketch details and top reader label to that,
+          this.updateSketchDetails(messageWord, messageNumber);
         }
         else {
           // correct word but incorrect number, so change readerlabel[top] to "invalid token"
-          console.log('invalid token. message: ', message);
+          console.log('invalid token number for reader: ', messageWord);
+          console.log('token number:', messageNumber);
           let updatedReaderLabels = this.state.readerLabels;
-          updatedReaderLabels.top = "Invalid Token";
+          updatedReaderLabels[messageWord] = "Invalid Token";
           this.setState({
             readerLabels: updatedReaderLabels,
           })
-        }
-        break;
-      default:
-        console.log("invalid messageWord")
-    }    
+        }  
+      }
+      else {
+        console.log('inavlid message word: ', messageWord);
+      }
+    }
+    else {
+      console.log('bad message: ', message)
+    }
   }
 
 
@@ -113,11 +126,13 @@ class App extends Component {
           sketchDetails={this.state.sketchDetails}
           onReaderMessage={this.onReaderMessage}
         />
-
-        <SettingsZone
-          readerLabels={this.state.readerLabels}
-        />
-
+        <div className="settings-zone">
+          <div className="readers-wrapper">
+            <SettingsZone
+              readerLabels={this.state.readerLabels}
+            />
+          </div>
+        </div>
       </div>
     );
   }
